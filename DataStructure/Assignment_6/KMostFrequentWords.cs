a921d4af8dd0b92549f771e9cc1a4b0c176882c0
AnshumanSinghh <2801anshuman@gmail.com>
2022-10-07 23:58:08 +0530

    [KMostFrequentWords]: lexicographically sorting logic corrected.

--- DataStructure/Assignment_6/KMostFrequentWords.cs
+++ DataStructure/Assignment_6/KMostFrequentWords.cs
@@ -20,8 +20,6 @@ namespace DataStructure.Assignment_6
                 kMostFrequentWordsInSortedOrder.Add(keyValuePairsList[0].Key);
                 keyValuePairsList.RemoveAt(0);
             }
-            
-            kMostFrequentWordsInSortedOrder = kMostFrequentWordsInSortedOrder.OrderBy(x => x[0]).ToList();
 
             // Now Print the 'K' most frequent words
             Console.Write($"'{k}' most frequent words: ");
@@ -65,20 +63,42 @@ namespace DataStructure.Assignment_6
             var left_child_idx = 2 * largest_idx + 1;
             var right_child_idx = 2 * largest_idx + 2;
 
-            if ((left_child_idx < n) && (keyValuePairsList[left_child_idx].Value > keyValuePairsList[parent_idx].Value))
+            if (left_child_idx < n) 
             {
-                parent_idx = left_child_idx;
+                if (keyValuePairsList[left_child_idx].Value > keyValuePairsList[parent_idx].Value)
+                {
+                    parent_idx = left_child_idx;
+                }
+                else if (keyValuePairsList[left_child_idx].Value == keyValuePairsList[parent_idx].Value)
+                {
+                    SortLexicographically(ref keyValuePairsList, left_child_idx, parent_idx);
+                }
             }
 
-            if ((right_child_idx < n) && (keyValuePairsList[right_child_idx].Value > keyValuePairsList[parent_idx].Value))
+            if (right_child_idx < n) 
             {
-                parent_idx = right_child_idx;
+                if (keyValuePairsList[right_child_idx].Value > keyValuePairsList[parent_idx].Value)
+                {
+                    parent_idx = right_child_idx;
+                }
+                else if (keyValuePairsList[right_child_idx].Value == keyValuePairsList[parent_idx].Value)
+                {
+                    SortLexicographically(ref keyValuePairsList, right_child_idx, parent_idx);
+                }
             }
 
             if (parent_idx != largest_idx) // Means left_child or right child is greater than parent.
             {
                 (keyValuePairsList[parent_idx], keyValuePairsList[largest_idx]) = (keyValuePairsList[largest_idx], keyValuePairsList[parent_idx]);  // Swap keyValuePairsList elements
                 Heapify(ref keyValuePairsList, parent_idx, n);
+            }
+        }
+
+        private void SortLexicographically(ref List<KeyValuePair<string, int>> keyValuePairsList, int child_idx, int parent_idx)
+        {
+            if (keyValuePairsList[child_idx].Key.CompareTo(keyValuePairsList[parent_idx].Key) < 0)
+            {
+                (keyValuePairsList[parent_idx], keyValuePairsList[child_idx]) = (keyValuePairsList[child_idx], keyValuePairsList[parent_idx]);  // only Swap keyValuePairsList elements
             }
         }
     }
